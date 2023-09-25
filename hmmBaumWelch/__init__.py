acff8836842880e8347a24900751510b228f5d40
George Stanley <George.Stanley@ukhsa.gov.uk>
2023-09-25 15:13:33 +0100

    Simplified init pavkage

--- hmmBaumWelch/__init__.py
+++ hmmBaumWelch/__init__.py
@@ -1,7 +1,4 @@
-__import__("pkg_resources").declare_namespace(__name__)
-
 from .BaumWelch import BaumWelch
-from .PriorDistributions import PriorDistributionArrays
 
 # add imports to namespace
-__all__ = ["BaumWelch", "PriorDistributionArrays"]
+__all__ = ["BaumWelch"]
